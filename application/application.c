687d9178dc348782a70d0a734d15105326538226
lurk101 <lurkone0one@gmail.com>
2021-11-28 12:52:44 -0500

    auto detect USB or serial console

--- application/application.c
+++ application/application.c
@@ -6,16 +6,36 @@
 
 #include "pico/multicore.h" // get_core_num()
 #include "pico/stdlib.h"
+#include "tusb.h"
 
 #include "crash.h"
 #include "stdio_cli.h"
 
 static void prvLaunchRTOS() {}
 
+void stdio_init(void) {
+    gpio_init(PICO_DEFAULT_UART_RX_PIN);
+    gpio_set_pulls(PICO_DEFAULT_UART_RX_PIN, 1, 0);
+    sleep_ms(1);
+    bool v1 = gpio_get(PICO_DEFAULT_UART_RX_PIN);
+    gpio_set_pulls(PICO_DEFAULT_UART_RX_PIN, 0, 1);
+    sleep_ms(1);
+    bool v2 = gpio_get(PICO_DEFAULT_UART_RX_PIN);
+    gpio_set_pulls(PICO_DEFAULT_UART_RX_PIN, 0, 0);
+    if (v1 != v2) {
+        stdio_usb_init();
+        while (!tud_cdc_connected())
+            sleep_ms(1000);
+    } else {
+        stdio_uart_init();
+        getchar_timeout_us(1000);
+    }
+}
+
 int main() {
 
     crash_handler_init();
-    stdio_init_all();
+    stdio_init();
     FreeRTOS_time_init();
     CLI_Start();
 
